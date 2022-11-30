bff83b713d7e2491df4ff3c833fe0201080929e9
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2022-11-30 02:41:23 +0100

    Add files via upload

---
+++ SimpleCookieClickerSpeedCoding/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace SimpleCookieClickerSpeedCoding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTabPage2 = new MetroFramework.Controls.MetroTabPage();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            this.metroTabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(24, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 253);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(24, 348);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(69, 19);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroLabel2.TabIndex = 4;
+            this.metroLabel2.Text = "Cookies: 0";
+            this.metroLabel2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.Controls.Add(this.metroTabPage2);
+            this.metroTabControl1.Location = new System.Drawing.Point(383, 27);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(410, 400);
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroTabControl1.TabIndex = 5;
+            this.metroTabControl1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.metroLabel3);
+            this.metroTabPage1.Controls.Add(this.metroButton3);
+            this.metroTabPage1.Controls.Add(this.metroLabel1);
+            this.metroTabPage1.Controls.Add(this.metroButton1);
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(402, 361);
+            this.metroTabPage1.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Shop";
+            this.metroTabPage1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(177, 47);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(150, 19);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroLabel3.TabIndex = 6;
+            this.metroLabel3.Text = "10 Cookies | Current: 0.0";
+            this.metroLabel3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroLabel3.Click += new System.EventHandler(this.metroLabel3_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(3, 43);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(168, 23);
+            this.metroButton3.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroButton3.TabIndex = 5;
+            this.metroButton3.Text = "Add 0.1 Cookies per second";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(177, 18);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(73, 19);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroLabel1.TabIndex = 4;
+            this.metroLabel1.Text = "35 Cookies";
+            this.metroLabel1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(3, 14);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(168, 23);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Buy 30s x2  Cookies";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click_1);
+            // 
+            // metroTabPage2
+            // 
+            this.metroTabPage2.Controls.Add(this.metroButton2);
+            this.metroTabPage2.HorizontalScrollbarBarColor = true;
+            this.metroTabPage2.Location = new System.Drawing.Point(4, 35);
+            this.metroTabPage2.Name = "metroTabPage2";
+            this.metroTabPage2.Size = new System.Drawing.Size(402, 361);
+            this.metroTabPage2.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroTabPage2.TabIndex = 1;
+            this.metroTabPage2.Text = "Settings";
+            this.metroTabPage2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroTabPage2.VerticalScrollbarBarColor = true;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(3, 14);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(168, 23);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Reset all";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.metroTabControl1);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Yellow;
+            this.Text = "Simple Cookie Clicker";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            this.metroTabPage1.PerformLayout();
+            this.metroTabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ SimpleCookieClickerSpeedCoding/Form1.cs
@@ -0,0 +1,184 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCookieClickerSpeedCoding
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double Cookies = 0.0;
+        double PerSecondCookies = 0.0;
+        double PerSecondsCookieAddOnePrice = 10.0;
+        Boolean startedThread = false;
+        Boolean x2 = false;
+
+        async void startCookieClicker()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                if(PerSecondCookies > 0.0)
+                {
+                    Cookies = Cookies + PerSecondCookies;
+                    MethodInvoker inv = delegate
+                    {
+                        this.metroLabel2.Text = "Cookies: " + this.Cookies.ToString();
+                    };
+
+                    try
+                    {
+                        this.Invoke(inv);
+                    } catch
+                    {
+                        MessageBox.Show("An internal error occurred while adding cookies per second.", "Cookie Adding Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+        async void startX2()
+        {
+            while (true)
+            {
+                if(x2) {
+                    await Task.Delay(30000);
+                    x2 = false;
+                    MethodInvoker inv = delegate
+                    {
+                        this.metroButton1.Enabled = true;
+                    };
+
+                    try
+                    {
+                        this.Invoke(inv);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("An internal error occurred while enableing x2 Button.", "Cookie Button Enable Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+
+        }
+
+        private bool checkCoins(double needed)
+        {
+            if(Cookies == needed || Cookies > needed) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+            if (!startedThread)
+            {
+                startedThread = true;
+                Thread threadPerSecondCookies = new Thread(startCookieClicker);
+                threadPerSecondCookies.Start();
+
+                Thread threadX2 = new Thread(startX2);
+                threadX2.Start();
+
+            }
+
+            if (Cookies == 0)
+            {
+                Cookies = 1;
+            }
+            else
+            {
+                if(x2) {
+                    Cookies = Cookies + 2;
+                } else {
+                    Cookies = Cookies + 1;
+                }
+            }
+            metroLabel2.Text = "Cookies: " + Cookies.ToString();
+        }
+
+        private void metroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            if (checkCoins(PerSecondsCookieAddOnePrice)) {
+                Cookies = Cookies - PerSecondsCookieAddOnePrice;
+                PerSecondsCookieAddOnePrice = PerSecondsCookieAddOnePrice * 2;
+                PerSecondCookies = PerSecondCookies + 0.1;
+                metroLabel3.Text = PerSecondsCookieAddOnePrice + " Cookies | Current: " + PerSecondCookies;
+                metroLabel2.Text = "Cookies: " + Cookies.ToString();
+            } else {
+                MessageBox.Show("You cannot purchase this product because you do not have enough cookies.", "Not enough cookies", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void metroLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click_1(object sender, EventArgs e)
+        {
+            x2 = true;
+            if (checkCoins(35)) {
+                metroButton1.Enabled = false;
+                Cookies = Cookies - 35;
+                metroLabel2.Text = "Cookies: " + Cookies.ToString();
+            } else {
+                MessageBox.Show("You cannot purchase this product because you do not have enough cookies.", "Not enough cookies", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void metroCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton2_Click_1(object sender, EventArgs e)
+        {
+            Cookies = 0.0;
+            PerSecondCookies = 0.0;
+            PerSecondsCookieAddOnePrice = 10.0;
+            startedThread = false;
+            x2 = false;
+
+            metroLabel2.Text = "Cookies: " + Cookies.ToString();
+            metroLabel3.Text = PerSecondsCookieAddOnePrice + " Cookies | Current: " + PerSecondCookies;
+        }
+    }
+}
